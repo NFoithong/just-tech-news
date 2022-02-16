75de49c461ce788f940cbfd7a49496d1b099326e
Josh <jzieve@Joshs-iMac.local>
2022-02-16 17:55:54 -0500

    Create PUT Route for Voting on a Post

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
-
+const { Post, User, Vote } = require('../../models');
+const sequelize = require('../../config/connection');
 // get all users
 router.get('/', (req, res) => {
     console.log('=================');
@@ -59,6 +59,17 @@ router.post('/', (req, res) => {
             console.log(err);
             res.status(500).json(err);
         });
+});
+
+// PUT /api/posts/upvote
+router.put('/upvote', (req, res) => {
+    Vote.create({
+            user_id: req.body.user_id,
+            post_id: req.body.post_id
+        })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err =>
+            res.status(500).json(err));
 });
 
 // create an update post

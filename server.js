f6ccd01edc82c25d5d796d07710a6f4f7b1cdca7
NFoithong <n.foithong1983@gmail.com>
2022-02-22 01:52:32 -0500

    update server

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
+const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 // setup path to public for stylesheet and html
-const path = require('path');
 // setup handlebars.js
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
@@ -10,18 +10,22 @@ const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 // The express.static() method is a built in express.js middleware function that 
 // can take all of the contents of a folder and serve them as statis assets.
-app.use(express.static(path.join(__dirname, '/public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 // app.use(express.static(__dirname + '/public'));
+// app.use('/static', express.static(path.join(__dirname, 'public')))
+// app.use(express.static('public'));
+app.use(express.static(__dirname + '/public/style.css'));
+
 
 // turn on routes
 app.use(routes);
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
